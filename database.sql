4756baf5c04f09902bd07ddeed64f46f8019f1c2
gkkconan <conan.gkk@gmail.com>
2025-03-22 17:45:58 +0100

    chore(database.sql): add db creation

--- database.sql
+++ database.sql
@@ -1,3 +1,6 @@
+CREATE DATABASE IF NOT EXISTS it_lab_manager;
+USE it_lab_manager;
+
 CREATE TABLE IF NOT EXISTS users (
   id INT AUTO_INCREMENT PRIMARY KEY,
   username VARCHAR(255) NOT NULL UNIQUE,
@@ -8,16 +11,16 @@ CREATE TABLE IF NOT EXISTS users (
   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
--- INSERT INTO users (username, email, password, role)
--- VALUES
---   ('name', 'email@email.com', 'password', 'user'),
---   ('nick', 'nick@email.com', 'pasf4s1', 'user'),
---   ('james', 'james.sniper@email.com', 'K3k1l@2s', 'user')
--- ON DUPLICATE KEY UPDATE
---   email = VALUES(email),
---   password = VALUES(password),
---   role = VALUES(role),
---   updated_at = CURRENT_TIMESTAMP;
+INSERT INTO users (username, email, password, role)
+VALUES
+  ('name', 'email@email.com', '$2b$10$rXxKP4RdwisF44oamWtqfeET78GUBK3WhbCew9P.quFksyqqMnw0y', 'dev'), -- password: password
+  ('nick', 'nick@email.com', '$2b$10$rXxKP4RdwisF44oamWtqfeET78GUBK3WhbCew9P.quFksyqqMnw0y', 'admin'), -- password: password
+  ('james', 'james.sniper@email.com', '$2b$10$rXxKP4RdwisF44oamWtqfeET78GUBK3WhbCew9P.quFksyqqMnw0y', 'user') -- password: password
+ON DUPLICATE KEY UPDATE
+  email = VALUES(email),
+  password = VALUES(password),
+  role = VALUES(role),
+  updated_at = CURRENT_TIMESTAMP;
 
 
 CREATE TABLE IF NOT EXISTS materials (
@@ -30,12 +33,14 @@ CREATE TABLE IF NOT EXISTS materials (
 	shelf INT NOT NULL
 );
 
--- INSERT INTO materials (name, quantity, note, location)
--- VALUES
--- 	('STM32F401RE', 34, '', 'armadietto a destra, secondo scaffale'),
--- 	('ESP8266', 30, 'xx/xx/xx: tizio ha preso 2pz (classe X)', 'seconda scatola sopra l\'armadietto a destra'),
--- 	('sensore HX711', 5, 'xx/xx/xx: tizio ha preso 2pz (classe X)', 'scatola sensori')
--- ON DUPLICATE KEY UPDATE
--- 	quantity = VALUES(quantity),
--- 	note = VALUES(note),
--- 	location = VALUES(location);
+INSERT INTO materials (name, quantity, note, laboratory, locker, shelf)
+VALUES
+	('STM32F401RE', 34, '', 'informatica', 1, 2),
+	('ESP8266', 30, 'xx/xx/xx: tizio ha preso 2pz (classe X)', 'telecomunicazioni', 2, 3),
+	('sensore HX711', 5, 'xx/xx/xx: tizio ha preso 2pz (classe X)', 'fisica', 3, 4)
+ON DUPLICATE KEY UPDATE
+	quantity = VALUES(quantity),
+	note = VALUES(note),
+	laboratory = VALUES(laboratory),
+	locker = VALUES(locker),
+	shelf = VALUES(shelf);
